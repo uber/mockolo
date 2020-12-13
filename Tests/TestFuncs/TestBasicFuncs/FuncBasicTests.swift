2b95e15d5f4212e534c953c8e3b3e11fdfbe4565
Florian Fittschen <ffittschen@gmail.com>
2020-12-13 17:30:18 +0100

    Add tests

--- Tests/TestFuncs/TestBasicFuncs/FuncBasicTests.swift
+++ Tests/TestFuncs/TestBasicFuncs/FuncBasicTests.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XCTest
 
 class BasicFuncTests: MockoloTestCase {
 
@@ -47,6 +48,25 @@ class BasicFuncTests: MockoloTestCase {
         verify(srcContent: simpleFuncs,
                dstContent: simpleMockFuncMock,
                useTemplateFunc: true)
+    }
+
+    func testFileWriteBehavior() throws {
+        verify(srcContent: simpleFuncs,
+               dstContent: simpleFuncsMock)
+
+        let expectedAttributes = try FileManager.default.attributesOfItem(atPath: dstFilePath)
+        let expectedCreationDate = (expectedAttributes[.creationDate] as! Date).timeIntervalSinceReferenceDate
+        let expectedModificationDate = (expectedAttributes[.modificationDate] as! Date).timeIntervalSinceReferenceDate
+
+        verify(srcContent: simpleFuncs,
+               dstContent: simpleFuncsMock)
+
+        let attributes = try FileManager.default.attributesOfItem(atPath: dstFilePath)
+        let creationDate = (attributes[.creationDate] as! Date).timeIntervalSinceReferenceDate
+        let modificationDate = (attributes[.modificationDate] as! Date).timeIntervalSinceReferenceDate
+
+        XCTAssertEqual(creationDate, expectedCreationDate)
+        XCTAssertEqual(modificationDate, expectedModificationDate)
     }
 }
 
