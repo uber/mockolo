712cab15289b5a318d69cb92d95ce89f6d68ed42
Ellie Shin <ellie@uber.com>
2021-01-07 13:58:55 -0800

    Bump 1.3.0

--- Sources/Mockolo/Executor.swift
+++ Sources/Mockolo/Executor.swift
@@ -232,5 +232,5 @@ public struct Version {
     public let value: String
 
     /// The current Mockolo version.
-    public static let current = Version(value: "1.2.9")
+    public static let current = Version(value: "1.3.0")
 }
