e1d3e7b34ec44239d3eb022d109fd3dc9d6ade40
EspressoCup <39891581+EspressoCup@users.noreply.github.com>
2021-09-23 21:54:22 -0700

    Update Version.swift
    1.6.1

--- Sources/MockoloFramework/Version.swift
+++ Sources/MockoloFramework/Version.swift
@@ -3,5 +3,5 @@ public struct Version {
     public let value: String
 
     /// The current Mockolo version.
-    public static let current = Version(value: "1.6.0")
+    public static let current = Version(value: "1.6.1")
 }
