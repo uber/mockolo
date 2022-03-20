90001b16b348ae632192d5d331610b0c18a8f12a
uhooi <the_uhooi@yahoo.co.jp>
2022-03-21 00:40:27 +0900

    Update Version.swift

--- Sources/MockoloFramework/Version.swift
+++ Sources/MockoloFramework/Version.swift
@@ -3,5 +3,5 @@ public struct Version {
     public let value: String
 
     /// The current Mockolo version.
-    public static let current = Version(value: "1.6.3")
+    public static let current = Version(value: "1.7.0")
 }
