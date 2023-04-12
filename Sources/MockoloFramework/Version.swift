d884aa9f258aeba9a88a4399715247920d8dc4d4
Iceman <okamura@qoncept.co.jp>
2023-04-12 16:40:26 +0900

    Update version number

--- Sources/MockoloFramework/Version.swift
+++ Sources/MockoloFramework/Version.swift
@@ -3,5 +3,5 @@ public struct Version {
     public let value: String
 
     /// The current Mockolo version.
-    public static let current = Version(value: "1.8.1")
+    public static let current = Version(value: "1.8.2")
 }
