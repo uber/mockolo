0eba214455588086c996a473640d91ba27c5eb42
woxtu <woxtup@gmail.com>
2023-04-22 08:24:13 +0900

    Fix typos

--- Tests/TestTestableImports/TestableImportStatementsTests.swift
+++ Tests/TestTestableImports/TestableImportStatementsTests.swift
@@ -2,13 +2,13 @@ import Foundation
 
 class TestableImportStatementsTests: MockoloTestCase {
     
-    func testTesableImportStatements() {
+    func testTestableImportStatements() {
         verify(srcContent: testableImports,
                dstContent: testableImportsMock,
                testableImports: ["SomeImport1", "SomeImport2"])
     }
 
-    func testTesableImportStatementsWithOverlap() {
+    func testTestableImportStatementsWithOverlap() {
         verify(srcContent: testableImportsWithOverlap,
                dstContent: testableImportsWithOverlapMock,
                testableImports: ["SomeImport1"])
