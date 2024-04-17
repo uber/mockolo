87fbb3e3bc8675bb088935a18eb9912a2f0a1ba5
Iceman <okamura@qoncept.co.jp>
2024-04-17 17:04:25 +0900

    trimming

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .executable(name: "mockolo", targets: ["Mockolo"]),
         .library(name: "MockoloFramework", targets: ["MockoloFramework"]),
-        ],
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
@@ -20,7 +20,7 @@ let package = Package(
             dependencies: [
                 "MockoloFramework",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                ]),
+            ]),
         .target(
             name: "MockoloFramework",
             dependencies: [
@@ -35,6 +35,5 @@ let package = Package(
             ],
             path: "Tests"
         )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
