64d2ca76a69c70e7a0def3716da6ff846cd2ab14
Iceman <okamura@qoncept.co.jp>
2024-10-22 17:26:40 +0900

    Use SwiftSyntax 6

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "MockoloFramework", targets: ["MockoloFramework"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
         .executableTarget(
