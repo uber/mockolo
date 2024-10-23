b26086da074225204c6bde77b0f3f80719e7a34a
Iceman <okamura@qoncept.co.jp>
2024-10-23 13:56:00 +0900

    Add macos only artifact

--- bundle/make_artifactbundle.sh
+++ bundle/make_artifactbundle.sh
@@ -12,6 +12,7 @@ if [ "$VERSION" = "" ]; then
     exit 1
 fi
 
+# Archive universal artifacts
 mkdir -p mockolo.artifactbundle/mockolo/macos
 mkdir -p mockolo.artifactbundle/mockolo/linux/x86_64-unknown-linux-gnu
 mkdir -p mockolo.artifactbundle/mockolo/linux/aarch64-unknown-linux-gnu
@@ -23,3 +24,12 @@ tar -xzf mockolo.linux-aarch64.tar.gz -C mockolo.artifactbundle/mockolo/linux/aa
 sed 's/__VERSION__/'$VERSION'/g' $(dirname $0)/info.json > mockolo.artifactbundle/info.json
 
 zip -r ./mockolo.artifactbundle.zip ./mockolo.artifactbundle
+
+# Archive macOS artifacts
+mkdir -p mockolo-macos.artifactbundle/mockolo/macos
+
+tar -xzf mockolo.macos-universal.tar.gz -C mockolo-macos.artifactbundle/mockolo/macos/
+
+sed 's/__VERSION__/'$VERSION'/g' $(dirname $0)/info-macos.json > mockolo-macos.artifactbundle/info.json
+
+zip -r ./mockolo-macos.artifactbundle.zip ./mockolo-macos.artifactbundle
