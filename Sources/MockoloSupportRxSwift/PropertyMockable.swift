263cd049b2709d845e98591873bc35caf9aa6279
fummicc1 <fumiya.tennis1@gmail.com>
2025-04-13 00:15:24 +0900

    Fix AssocitaedType of Observable. Delete Internal MockObservable.

--- Sources/MockoloSupportRxSwift/PropertyMockable.swift
+++ Sources/MockoloSupportRxSwift/PropertyMockable.swift
@@ -14,32 +14,57 @@
 //  limitations under the License.
 //
 
-
-#if RxSwiftImported
-
 /**
  The following property wrappers may be used for RxSwift observable variables to be mocked.
- There are two, public and internal, since the access levels should match the properties declared.
 
  E.g.
  public class FooMock: Foo {
     @MockObservable  public var bazStream: Observable<Int> { get }
  }
-
- class BarMock: Bar {
-    @MockObservableInternal  var bazStream: Observable<Int> { get }
- }
  */
 
+#if canImport(RxSwift)
 import RxSwift
+#else
+// ---- Mimic of RxSwift types -----
+public protocol ObservableConvertibleType {
+    associatedtype Element
+    func asObservable() -> any ObservableType
+}
+public protocol ObservableType: ObservableConvertibleType {
+}
+
+public class Observable<Element>: ObservableType {
+    public func asObservable() -> any ObservableType {
+        fatalError()
+    }
+    public static func empty() -> Observable<Element> {
+        fatalError()
+    }
+}
+
+public class BehaviorSubject<Element>: Observable<Element> {
+    public init(value: Element) {}
+}
+public class PublishSubject<Element>: Observable<Element> {
+    public override init() {}
+}
+public class ReplaySubject<Element>: Observable<Element> {
+    public static func create(bufferSize: Int) -> ReplaySubject<Element> {
+        fatalError()
+    }
+}
+public class BehaviorRelay<Element>: Observable<Element> {}
+// ----
+#endif
 
 @propertyWrapper
 public struct MockObservable<Value: ObservableType> {
     var whichKind = 0
     public var callCount = 0
-    public var publishSubject: PublishSubject<Value.E> = PublishSubject<Value.E>()
-    public var replaySubject: ReplaySubject<Value.E> = ReplaySubject<Value.E>.create(bufferSize: 1)
-    public var behaviorSubject: BehaviorSubject<Value.E>!
+    public var publishSubject: PublishSubject<Value.Element> = PublishSubject<Value.Element>()
+    public var replaySubject: ReplaySubject<Value.Element> = ReplaySubject<Value.Element>.create(bufferSize: 1)
+    public var behaviorSubject: BehaviorSubject<Value.Element>!
     var fallback: Value!
     public init(wrappedValue: Value) {
         storeVal(wrappedValue)
@@ -59,13 +84,13 @@ public struct MockObservable<Value: ObservableType> {
     }
     
     private mutating func storeVal(_ newValue: Value) {
-        if let val = newValue as? PublishSubject<Value.E> {
+        if let val = newValue as? PublishSubject<Value.Element> {
             publishSubject = val
             whichKind = 0
-        } else if let val = newValue as? ReplaySubject<Value.E> {
+        } else if let val = newValue as? ReplaySubject<Value.Element> {
             replaySubject = val
             whichKind = 1
-        } else if let val = newValue as? BehaviorSubject<Value.E> {
+        } else if let val = newValue as? BehaviorSubject<Value.Element> {
             behaviorSubject = val
             whichKind = 2
         } else {
@@ -74,48 +99,3 @@ public struct MockObservable<Value: ObservableType> {
         }
     }
 }
-
-@propertyWrapper
-struct MockObservableInternal<Value: ObservableType> {
-    var callCount = 0
-    var whichKind = 0
-    var publishSubject: PublishSubject<Value.E> = PublishSubject<Value.E>()
-    var replaySubject: ReplaySubject<Value.E> = ReplaySubject<Value.E>.create(bufferSize: 1)
-    var behaviorSubject: BehaviorSubject<Value.E>!
-    var fallback: Value!
-    init(wrappedValue: Value) {
-        storeVal(wrappedValue)
-    }
-    
-    var wrappedValue: Value {
-        get {
-            if whichKind == 0 { return publishSubject as! Value  }
-            else if whichKind == 1 { return replaySubject as! Value  }
-            else if whichKind == 2 { return behaviorSubject as! Value  }
-            else { return fallback }
-        }
-        set {
-            storeVal(newValue)
-            callCount += 1
-        }
-    }
-    
-    private mutating func storeVal(_ newValue: Value) {
-        if let val = newValue as? PublishSubject<Value.E> {
-            publishSubject = val
-            whichKind = 0
-        } else if let val = newValue as? ReplaySubject<Value.E> {
-            replaySubject = val
-            whichKind = 1
-        } else if let val = newValue as? BehaviorSubject<Value.E> {
-            behaviorSubject = val
-            whichKind = 2
-        } else {
-            fallback = newValue
-            whichKind = 3
-        }
-    }
-}
-
-#endif
-
