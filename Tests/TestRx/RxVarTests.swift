9fbbae0642703aee6ce9904df569742c61eef89d
fummicc1 <fumiya.tennis1@gmail.com>
2025-04-13 00:17:21 +0900

    Use `@Fixture` macro in Tests.`

--- Tests/TestRx/RxVarTests.swift
+++ Tests/TestRx/RxVarTests.swift
@@ -3,34 +3,34 @@ import Foundation
 class RxVarTests: MockoloTestCase {
     
     func testRx() {
-        verify(srcContent: rx,
-               dstContent: rxMock)
+        verify(srcContent: rxTaskRouting._source,
+               dstContent: rxTaskRouting.expected._source)
     }
 
     func testRxMultiParents() {
-           verify(srcContent: rxMultiParents,
-                  dstContent: rxMultiParentsMock)
+        verify(srcContent: rxMultiParents._source,
+               dstContent: rxMultiParents.expected._source)
     }
 
     func testRxMultiParentsMockObservable() {
-           verify(srcContent: rxMultiParents,
-                  dstContent: rxMultiParentsMockObservable,
+        verify(srcContent: rxMultiParents._source,
+               dstContent: rxMultiParents.expected_useMockObservable._source,
                   useMockObservable: true)
     }
 
     func testRxVarInherited() {
-        verify(srcContent: rxVarInherited,
-               dstContent: rxVarInheritedMock)
+        verify(srcContent: rxInherited._source,
+               dstContent: rxInherited.expected._source)
     }
 
     func testRxObservables() {
-        verify(srcContent: rxObservables,
-               dstContent: rxObservablesMock)
+        verify(srcContent: rxObservables._source,
+               dstContent: rxObservables.expected._source)
     }
 
     func testRxSubjects() {
-        verify(srcContent: rxSubjects,
-               mockContent: rxSubjectsParent,
-               dstContent: rxSubjectsMock)
+        verify(srcContent: rxSubjects._source,
+               mockContent: rxSubjects.parent._source,
+               dstContent: rxSubjects.expected._source)
     }
 }
