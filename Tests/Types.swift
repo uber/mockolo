b2e28da41db03efdf36e382f6e7d4fa4d063e04b
fummicc1 <fumiya.tennis1@gmail.com>
2025-04-13 20:13:13 +0900

    Add Rx types for Testing

--- Tests/Types.swift
+++ Tests/Types.swift
@@ -1,3 +1,34 @@
 @MainActor class UIViewController {}
 struct CGImage {}
 struct CGRect {}
+
+// ---- Dummy RxSwift types -----
+public protocol ObservableConvertibleType {
+    associatedtype Element
+    func asObservable() -> any ObservableType
+}
+public protocol ObservableType: ObservableConvertibleType {
+}
+
+public class Observable<Element>: ObservableType {
+    public func asObservable() -> any ObservableType {
+        fatalError()
+    }
+    public static func empty() -> Observable<Element> {
+        fatalError()
+    }
+}
+
+public class BehaviorSubject<Element>: Observable<Element> {
+    public init(value: Element) {}
+}
+public class PublishSubject<Element>: Observable<Element> {
+    public override init() {}
+}
+public class ReplaySubject<Element>: Observable<Element> {
+    public static func create(bufferSize: Int) -> ReplaySubject<Element> {
+        fatalError()
+    }
+}
+public class BehaviorRelay<Element>: Observable<Element> {}
+// ----
