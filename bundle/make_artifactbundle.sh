cf6b6e61679202b7cb7c73a5710ca49a68fcf5fa
kenta okamura <side.junktown@gmail.com>
2025-05-03 21:28:29 +0900

    Drop macos only artifact

--- bundle/make_artifactbundle.sh
+++ bundle/make_artifactbundle.sh
@@ -24,12 +24,3 @@ tar -xzf mockolo.ubuntu-aarch64.tar.gz -C mockolo.artifactbundle/mockolo/ubuntu/
 sed 's/__VERSION__/'$VERSION'/g' $(dirname $0)/info.json > mockolo.artifactbundle/info.json
 
 zip -r ./mockolo.artifactbundle.zip ./mockolo.artifactbundle
-
-# Archive macOS only artifacts
-mkdir -p mockolo-macos.artifactbundle/mockolo/macos
-
-tar -xzf mockolo.macos-universal.tar.gz -C mockolo-macos.artifactbundle/mockolo/macos/
-
-sed 's/__VERSION__/'$VERSION'/g' $(dirname $0)/info-macos.json > mockolo-macos.artifactbundle/info.json
-
-zip -r ./mockolo-macos.artifactbundle.zip ./mockolo-macos.artifactbundle
