43ce53f252fdc32b2ac317fdaa6147e6ff83906c
Paul Berman <pberman@slack-corp.com>
2026-03-11 13:02:50 -0500

    Test we handle inheritance properly.

--- Tests/TestOverrides/FixtureMockNames.swift
+++ Tests/TestOverrides/FixtureMockNames.swift
@@ -23,3 +23,92 @@ class FooMock: FooProtocol {
     }
 }
 """
+
+func mockableDeclaration(customName: String?) -> String {
+    switch customName {
+    case .some(let customName):
+        "/// @mockable(override: name = \(customName))"
+    case .none:
+        "/// @mockable()"
+    }
+}
+
+func baseProtocol(customName: String?) -> String {
+    """
+    \(mockableDeclaration(customName: customName))
+    protocol BaseProtocol {
+        func register()
+        var counter: Int { get } 
+    }
+    """
+}
+
+func baseMock(customName: String?) -> String {
+    """
+    class \(customName ?? "BaseProtocolMock"): BaseProtocol {
+        init() { }
+        init(counter: Int = 0) {
+            self.counter = counter
+        }
+        private(set) var registerCallCount = 0
+        var registerHandler: (() -> ())?
+        func register() {
+            registerCallCount += 1
+            if let registerHandler = registerHandler {
+                registerHandler()
+            }
+        }
+        var counter: Int = 0
+    }
+    """
+}
+
+func derivedProtocol(customName: String?) -> String {
+    """
+    \(mockableDeclaration(customName: customName))
+    protocol DerivedProtocol : BaseProtocol {
+        func like()
+        func subscribe() 
+    }
+    """
+}
+
+func derivedMock(customName: String?) -> String {
+    """
+    class \(customName ?? "DerivedProtocolMock"): DerivedProtocol {
+        init() { }
+        init(counter: Int = 0) {
+            self.counter = counter
+        }
+
+        private(set) var likeCallCount = 0
+        var likeHandler: (() -> ())?
+        func like() {
+            likeCallCount += 1
+            if let likeHandler = likeHandler {
+                likeHandler()
+            }
+            
+        }
+
+        private(set) var subscribeCallCount = 0
+        var subscribeHandler: (() -> ())?
+        func subscribe() {
+            subscribeCallCount += 1
+            if let subscribeHandler = subscribeHandler {
+                subscribeHandler()
+            }
+            
+        }
+        private(set) var registerCallCount = 0
+        var registerHandler: (() -> ())?
+        func register() {
+            registerCallCount += 1
+            if let registerHandler = registerHandler {
+                registerHandler()
+            }
+        }
+        var counter: Int = 0
+    }
+    """
+}
--- Tests/TestOverrides/NameOverrideTests.swift
+++ Tests/TestOverrides/NameOverrideTests.swift
@@ -4,4 +4,27 @@ class NameOverrideTests: MockoloTestCase {
     func testNameOverride() {
         verify(srcContent: nameOverride, dstContent: nameOverrideMock)
     }
+    
+    let baseCustomizations: [String?] = [nil, "BaseProtocolMock", "BaseMock", "FakeBase"]
+    let derivedCustomizations: [String?] = [nil, "DerivedProtocolMock", "DerivedMock", "FakeDerived"]
+    
+    func testBaseFixtures() {
+        for baseCustomization in baseCustomizations {
+            verify(srcContent: baseProtocol(customName: baseCustomization), dstContent: baseMock(customName: baseCustomization))
+        }
+    }
+    
+    func testDerivedFixtures() {
+        for baseCustomization in baseCustomizations {
+            let baseImplementation = baseMock(customName: baseCustomization)
+            for derivedCustomization in derivedCustomizations {
+                verify(
+                    srcContent: derivedProtocol(customName: derivedCustomization),
+                    mockContent: baseImplementation,
+                    dstContent: derivedMock(customName: derivedCustomization)
+                )
+            }
+        }
+    }
+    
 }
